ec7bfb517cbbe6d8084c6e23b9234783284dc927
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2021-08-20 15:02:51 -0500

    If the user has a lab manager or pi role with an RC organization, set them to a Manager for the Project with the RC group name

--- coldfront/plugins/ifx/nanites.py
+++ coldfront/plugins/ifx/nanites.py
@@ -13,6 +13,7 @@ All rights reserved.
 
 import logging
 from ifxuser.nanites import Nanite2User
+from coldfront.core.project import Project, ProjectUser, ProjectUserRoleChoice
 
 
 logger = logging.getLogger(__name__)
@@ -29,3 +30,19 @@ class Nanite2ColdfrontUser(Nanite2User):
         if role == 'coldfront_user':
             user.is_active = False
             user.save()
+        if role == 'coldfront_pi':
+            # Go through affiliations.  If PI or Lab Manager, set as Manager of corresponding project
+            for ua in user.useraffiliation_set.filter(role__in=['pi', 'lab_manager']):
+                lab = ua.organization
+                if lab.org_tree == 'Research Computing AD' and lab.code:
+                    # Find project name that matches Organization.code
+                    try:
+                        project = Project.objects.get(name=lab.code)
+                        try:
+                            project_user = ProjectUser.objects.get(user=user, project=project)
+                        except ProjectUser.DoesNotExist:
+                            project_user= ProjectUser(user=user, project=project)
+                        project_user.role = ProjectUserRoleChoice.objects.get(name='Manager')
+                        project_user.save()
+                    except Project.DoesNotExist:
+                        logger.info(f'No Project matches RC organization code {lab.code}')
