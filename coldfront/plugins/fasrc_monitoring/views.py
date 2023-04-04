71c5c6786ca47dd61e81e628a04962e0dcf0f889
Claire Peters <clairepeters@g.harvard.edu>
2023-04-04 10:26:35 -0700

    improve handling of missing data on monitoring page

--- coldfront/plugins/fasrc_monitoring/views.py
+++ coldfront/plugins/fasrc_monitoring/views.py
@@ -11,18 +11,24 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 
+from coldfront.config.env import ENV
 from coldfront.core.project.models import Project
 from coldfront.core.allocation.models import Allocation
-from coldfront.plugins.sftocf.utils import STARFISH_SERVER, StarFishServer
+
+if ENV.bool('PLUGIN_SFTOCF', default=False):
+    from coldfront.plugins.sftocf.utils import STARFISH_SERVER, StarFishServer
 
 @login_required
 def monitor(request):
     '''
 
     '''
-    sf = StarFishServer(STARFISH_SERVER)
-    scan_data = sf.get_most_recent_scans()
-    scan_data_processed = [
+    scan_data_processed = None
+    if ENV.bool('PLUGIN_SFTOCF', default=False):
+
+        sf = StarFishServer(STARFISH_SERVER)
+        scan_data = sf.get_most_recent_scans()
+        scan_data_processed = [
                     {'volume': s['volume'],
                      'state': s['state']['name'],
                      'creation_time_hum': s['creation_time_hum'],
@@ -35,9 +41,13 @@ def monitor(request):
     pi_not_projectuser = [p for p in projects if p.pi_id not in  p.projectuser_set.values_list('user_id', flat=True)]
     allocation_not_changeable = Allocation.objects.filter(is_changeable=False)
 
-    page_issues_ts = os.path.getmtime('local_data/error_checks.csv')
-    page_issues_dt = datetime.fromtimestamp(page_issues_ts)
-    page_issues = pd.read_csv('local_data/error_checks.csv').to_dict('records')
+    ui_error_file = 'local_data/error_checks.csv'
+    page_issues_dt = None
+    page_issues = None
+    if os.path.isfile(ui_error_file):
+        page_issues_ts = os.path.getmtime(ui_error_file)
+        page_issues_dt = datetime.fromtimestamp(page_issues_ts)
+        page_issues = pd.read_csv(ui_error_file).to_dict('records')
 
     template_name = 'monitor.html'
     context = {'scan_data': scan_data_processed,
