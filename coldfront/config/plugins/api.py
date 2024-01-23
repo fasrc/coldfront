6fff234fb5b6b5f3631fb8a46e008949858baa30
claire-peters <clairepeters@g.harvard.edu>
2024-01-23 14:56:37 -0800

    update api plugin config file

--- coldfront/config/plugins/api.py
+++ coldfront/config/plugins/api.py
@@ -3,5 +3,12 @@ from coldfront.config.base import INSTALLED_APPS
 INSTALLED_APPS += ['coldfront.plugins.api']
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ]
 }
