e08bf682212bfb0d2c02776bc42cd549b8aa1065
Claire Peters <clairepeters@g.harvard.edu>
2025-09-23 14:18:38 -0700

    change token reference

--- coldfront/plugins/slurmrest/utils.py
+++ coldfront/plugins/slurmrest/utils.py
@@ -24,7 +24,7 @@ class SlurmApiConnection():
 
     def _return_configuration(self):
         configuration = Configuration(host=self.active_cluster.get('base_url'))
-        configuration.api_key['token'] = self.active_cluster.get('user_token')
+        configuration.api_key['token'] = self.active_cluster.get('token')
         return configuration
 
     def _return_apiconn(self, api):
