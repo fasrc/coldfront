d42a8b97fbb24bd524edf26068c2587c88c9e5a4
Claire Peters <clairepeters@g.harvard.edu>
2025-10-03 17:49:44 -0700

    fix signals

--- coldfront/plugins/slurm/signals.py
+++ coldfront/plugins/slurm/signals.py
@@ -81,8 +81,6 @@ def allocation_activate_user_handler(sender, **kwargs):
         Q(resourceattribute__resource_attribute_type__name='slurm_cluster') &
         Q(resourceattribute__value=kwargs.get('cluster'))
     )
-    slurm_cluster = Resource.objects.get(
-        resourceattribute__name='slurm_cluster', resourceattribute__value=kwargs.get('cluster'))
     if not slurm_cluster or slurm_cluster.get_attribute('slurm_integration') != 'CLI':
         return
     allocationuser = AllocationUser.objects.get(pk=kwargs['allocation_user_pk'])
