572ef8d3b247af8faed580bff367010ebfa5d56d
Claire Peters <clairepeters@g.harvard.edu>
2025-11-05 17:58:32 -0800

    remove empty parameter from post_assoc

--- coldfront/plugins/slurmrest/signals.py
+++ coldfront/plugins/slurmrest/signals.py
@@ -70,7 +70,7 @@ def slurmrest_allocation_raw_share_edit_handler(sender, **kwargs):
     if not slurm_cluster or slurm_cluster.get_attribute('slurm_integration') != 'API':
         return
     api = SlurmApiConnection(slurm_cluster.get_attribute('slurm_cluster'))
-    api.post_assoc(kwargs['account'], "", None, {'shares_raw': str(kwargs['raw_share'])})
+    api.post_assoc(kwargs['account'], "", {'shares_raw': str(kwargs['raw_share'])})
 
 
 @receiver(allocation_activate_user)
