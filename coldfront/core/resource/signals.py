3df0930f583614b1c594a1f3bd95dc04f3de5f8d
Claire Peters <clairepeters@g.harvard.edu>
2026-03-11 17:42:29 -0700

    use signal for isilon volumes in pull_resource_data

--- coldfront/core/resource/signals.py
+++ coldfront/core/resource/signals.py
@@ -5,3 +5,5 @@ resource_apicluster_table_data_request = django.dispatch.Signal()
 
 resource_clicluster_table_data_request = django.dispatch.Signal()
 # [resource, allocations]
+
+update_volume_information = django.dispatch.Signal()
--- coldfront/plugins/isilon/signals.py
+++ coldfront/plugins/isilon/signals.py
@@ -5,12 +5,40 @@ from coldfront.core.allocation.signals import (
         allocation_autocreate,
         allocation_autoupdate,
 )
+from coldfront.core.resource.models import ResourceAttributeType
+from coldfront.core.resource.signals import update_volume_information
 from coldfront.plugins.isilon.utils import (
+    IsilonConnection,
     create_isilon_allocation_quota,
     update_isilon_allocation_quota,
 )
 
 logger = logging.getLogger(__name__)
+
+@receiver(update_volume_information)
+def update_isilon_volume_information(sender, **kwargs):
+
+    resource = kwargs['resource']
+    if 'isilon' in resource.name:
+        resource_name = resource.name.split('/')[0]
+        isilon_api = IsilonConnection(resource_name)
+        isilon_capacity_tb = isilon_api.to_tb(isilon_api.total_space)
+        isilon_free_tb = isilon_api.to_tb(isilon_api.unused_space)
+        isilon_allocated_tb = isilon_api.to_tb(isilon_api.allocated_space)
+        isilon_used_tb = isilon_api.to_tb(isilon_api.used_space)
+        attr_pairs = {
+            'capacity_tb': isilon_capacity_tb,
+            'allocated_tb': isilon_allocated_tb,
+            'free_tb': isilon_free_tb,
+            'used_tb': isilon_used_tb,
+        }
+        for attr_name, attr_val in attr_pairs.items():
+            if attr_val:
+                attr_type_obj = ResourceAttributeType.objects.get(name=attr_name)
+                resource.resourceattribute_set.update_or_create(
+                    resource_attribute_type=attr_type_obj,
+                    defaults={'value': attr_val}
+                )
 
 @receiver(allocation_autocreate)
 def activate_allocation(sender, **kwargs):
